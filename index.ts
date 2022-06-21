7e12b8e502bd714543841e8a84eff2ea50a87154
agencik007 <agencik007@gmail.com>
2022-06-21 20:49:55 +0200

    Added the necessary packages [bcryptjs, dotenv, express-async-handler, express-rate-limiter and types] Added error handler Added config and connection to MongoDB database

--- index.ts
+++ index.ts
@@ -1,6 +1,16 @@
-import express, {json} from 'express';
+import express, {json, Router} from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+
+import {connectDB} from "./config/db";
+import rateLimit from 'express-rate-limit';
+import {beerRouter} from "./routes/beerRouter";
+import {handleError} from "./utlis/errors";
+import 'dotenv/config';
+
+(async () => {
+    await connectDB();
+})();
 
 const app = express();
 
@@ -9,6 +19,21 @@ app.use(cors({
 }));
 
 app.use(json());
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}));
+
+const router = Router();
+
+router.use('/beers', beerRouter);
+
+app.use('/api', router);
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on http://localhost:3001');
