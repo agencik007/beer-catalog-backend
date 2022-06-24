88c0d62190b7a0cf22b1534be538eccf9cb692e4
agencik007 <agencik007@gmail.com>
2022-06-24 23:57:22 +0200

    Added user type

--- types/user/user.entity.ts
+++ types/user/user.entity.ts
@@ -1,10 +1,7 @@
-import mongoose from "mongoose";
-
 export interface UserEntity {
-    id?: mongoose.Schema.Types.ObjectId;
-    _id?: mongoose.Schema.Types.ObjectId;
-    name?: string;
+    id?: string;
+    name: string;
     email: string;
     password: string;
-    role?: 'user' | 'admin';
+    role: 'user' | 'admin';
 }
